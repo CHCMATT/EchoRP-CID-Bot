579d0c60cf8e7f19c1ca648bc012e3fd6afee447
Matthew Anthony <misterbaboo@gmail.com>
2023-04-13 22:46:05 -0400

    Reorder requirements

--- editEmbed.js
+++ editEmbed.js
@@ -1,5 +1,5 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
 const dbCmds = require('./dbCmds.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
 
 const formatter = new Intl.NumberFormat('en-US', {
 	style: 'currency',
