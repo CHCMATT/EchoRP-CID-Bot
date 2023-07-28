3ae0ee2067cd5ba5f480aebb2fd4f22ee9acf2cc
Matthew Anthony <misterbaboo@gmail.com>
2023-07-27 22:10:29 -0400

    Fix order of ops for error logging, fix discriminator showing in certain commands

--- commands/formatName.js
+++ commands/formatName.js
@@ -32,35 +32,32 @@ module.exports = {
 					const charName = interaction.options.getString('name');
 					const newNick = callSign + " | " + charName;
 					await interaction.guild.members.cache.get(user.id).setNickname(`${newNick}`, `Requested by: ${interaction.member.user.username}`);
-					await interaction.reply({ content: `Successfully changed nickname of \`${user.username}#${user.discriminator}\` to \`${newNick}\`.`, ephemeral: true });
+					await interaction.reply({ content: `Successfully changed nickname of \`${user.username}\` to \`${newNick}\`.`, ephemeral: true });
 				}
 				catch (error) {
 					const user = interaction.options.getUser('user');
-					await interaction.reply({ content: `:warning: Unable to change nickname - my highest role isn't higher than \`${user.username}#${user.discriminator}\`'s.`, ephemeral: true });
+					await interaction.reply({ content: `:warning: Unable to change nickname - my highest role isn't higher than \`${user.username}\`'s.`, ephemeral: true });
 				}
 			}
 			else {
 				await interaction.reply({ content: `:x: You must have the \`Administrator\` permission to use this function.`, ephemeral: true });
 			}
 		} catch (error) {
-			if (process.env.BOT_NAME == 'test') {
-				console.error(error);
-			} else {
-				console.log(`Error occured at ${errTime} at file ${fileName}!`);
-				console.error(error);
+			console.error(error);
 
-				let errTime = moment().format('MMMM Do YYYY, h:mm:ss a');;
-				let fileParts = __filename.split(/[\\/]/);
-				let fileName = fileParts[fileParts.length - 1];
+			let errTime = moment().format('MMMM Do YYYY, h:mm:ss a');;
+			let fileParts = __filename.split(/[\\/]/);
+			let fileName = fileParts[fileParts.length - 1];
 
-				let errorEmbed = [new EmbedBuilder()
-					.setTitle(`An error occured on the ${process.env.BOT_NAME} bot file ${fileName}!`)
-					.setDescription(`\`\`\`${error.toString().slice(0, 2000)}\`\`\``)
-					.setColor('B80600')
-					.setFooter({ text: `${errTime}` })];
+			console.log(`Error occured at ${errTime} at file ${fileName}!`);
 
-				await interaction.client.channels.cache.get(process.env.ERROR_LOG_CHANNEL_ID).send({ embeds: errorEmbed });
-			}
+			let errorEmbed = [new EmbedBuilder()
+				.setTitle(`An error occured on the ${process.env.BOT_NAME} bot file ${fileName}!`)
+				.setDescription(`\`\`\`${error.toString().slice(0, 2000)}\`\`\``)
+				.setColor('B80600')
+				.setFooter({ text: `${errTime}` })];
+
+			await interaction.client.channels.cache.get(process.env.ERROR_LOG_CHANNEL_ID).send({ embeds: errorEmbed });
 		}
 	},
 };
